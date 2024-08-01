00f020906ef7120874b3760e7cf356e6ccd0ac30
lilwondermelo <lilwondermelo@yandex.ru>
2024-08-01 15:48:38 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import Game from './Model/Game.js';
 const app = window.Telegram.WebApp;
 app.ready()
+app.expand()
 app.isVerticalSwipesEnabled = false;
 // Добавляем таблицу на страницу
 let game = new Game(5, 8);
